43805cf0133860642a40a2d7fdf4094bae1af08b
Jesse Bloecker <jesseb@udel.edu>
2023-10-08 23:52:57 -0400

    add timescale variable, load params from yaml

--- src/bspline_controller.py
+++ src/bspline_controller.py
@@ -1,77 +1,74 @@
 #!/usr/bin/env python 
 
-
 import numpy as np
 from time import sleep
 from pynput import keyboard
 import time
 from bspline_core import BSplineCore
+import motion_utils as utils
+import threading
+import yaml
 
 
 class BSplineController():
-    def __init__(self):
-        """
-        Adjust the position of any control points in the bspline and other params via key presses or UI buttons
-        other features:
-        change the scale of the whole curve - expand about centroid or selected point
-        min max velocity and acceleration, centroid, path length, path duration
-        """
-        self.control_pts = np.array([[0,0,0],  #idk where this will be set by the user yet
-                                [0,0,0],
-                                [3,0,0],
-                                [16,7,0],
-                                [12,14,0],
-                                [7,1,0],
-                                [0,20,0],
-                                [-10,17,0],
-                                [-3,8,0],
-                                [-2,0,0],
-                                [-2,0,0]]).astype(np.float64)
-        self.control_pts_orig = self.control_pts.copy()
-        self.selection = 0 #index of the currently selected control point
+    """
+    Control the position of any control points in the bspline and other params via key presses or UI buttons
+    and compute the bspline and its derivatives
 
+    Attributes:
+        core (BSplineCore): core instance for computing the bspline
+        timescale (float): time (seconds) that it takes to travel 1 span of the spline (use this change speed)
+        increment (float): amount (meters) by which to move selected control point on key press
+        control_pts (ndarray): n x 3 array of current control points
+        selection (int): index of the currently selected control point TODO: handle repeated control points better
+        directions (dict): directions to move corresponding to key presses
+        shift_active (bool): flag for shift key
+        min_update_time (float): minimum time required to recompute the bspline
+        last_update (float): time of last update
+        pos_bspline (ndarray): n x 3 array of position values
+        vel_bspline (ndarray): n x 3 array of velocity values
+        acc_bspline (ndarray): n x 3 array of acceleration values
+    """
+    def __init__(self):
+
+        #Spline parameters - load from yaml
+        config_path = '/home/jesse/ros2_ws/src/motion_tools/config/bspline_config.yaml'
+        res = yaml.load(open(config_path),Loader=yaml.FullLoader)['res']
+        order = yaml.load(open(config_path),Loader=yaml.FullLoader)['order']
+        timescale = yaml.load(open(config_path),Loader=yaml.FullLoader)['timescale']
+        increment = yaml.load(open(config_path),Loader=yaml.FullLoader)['increment']
+        control_pts = yaml.load(open(config_path),Loader=yaml.FullLoader)['control_pts']
+        control_pts = np.array(control_pts).astype(np.float64)
+
+        control_pts_reduced, inds = np.unique(control_pts,axis=0, return_index=True) #use np.diff to get rid of repeated control points
+        control_pts_reduced = control_pts_reduced[np.argsort(inds)]
+        print('control_pts_reduced',control_pts_reduced)
+        duration = timescale*(len(control_pts)-order) #total duration of the bspline
+        
+        self.duration_sec = duration
+        self.timescale = timescale
+        self.increment = increment
+        self.control_pts = control_pts
+        self.control_pts_orig = self.control_pts.copy()
+        self.selection = 0  #index of the currently selected control point
         self.directions = {'d':np.array([1.,0.,0.]),  # +x    directions to move corresponding to key presses
                            'a':np.array([-1.,0.,0.]), # -x
                            'w':np.array([0.,1.,0.]),  # +y
                            's':np.array([0.,-1.,0.]), # -y
                            'q':np.array([0.,0.,1.]),  # +z
                            'e':np.array([0.,0.,-1.])} # -z
-        
-        #spline params. UI can change at any time
-        r=100
-        self.resolution = r #number of points in the bspline 
-        self.order = 3 #order of the bspline 
-        self.t = np.linspace(0,1,num=r,endpoint=False) #default
-
-
-        self.increment=0.1 #amount to move selected control point by
         self.shift_active = False #flag for shift key
-        self.min_update_time = 0.5 #minimum time between recomputing the bspline
+        self.min_update_time = 0.5 #minimum time required to recompute the bspline
+        self.update_waiting = False
         self.last_update = time.time() #time of last update
 
-        #store all of the splines, recompute on key press
-        self.pos_bspline = None 
-        self.vel_bspline = None
-        self.acc_bspline = None
-        self.jerk_bspline = None     
-        self.snap_bspline = None   
-
-        r = self.resolution
-        t = self.t
-        order = self.order
-        control_pts = self.control_pts
-
-
-        core = BSplineCore(t,order) #core class for computing the bspline
+        core = BSplineCore(res,order) #core class for computing the bspline
         self.core=core
-        #pos, vel, acc, jerk, snap, 5th deriv, 6th deriv. Some will have value of None (unless order is the max) 
-        # p,v,a,j,s,d5,d6 = self.compute_bsplines(core,control_pts) 
         p,v,a = self.compute_bsplines(core,control_pts) #for now, just compute the first 3 derivatives
         self.pos_bspline = p
         self.vel_bspline = v
         self.acc_bspline = a
-
-    
+        self.update()
 
     def update_control_pt(self,k):
         """
@@ -85,7 +82,6 @@ class BSplineController():
         new_pos = cur_pos + self.directions[k]*inc
         self.control_pts[s]=new_pos
 
-    
     def select(self):
         """
         select a control point
@@ -97,7 +93,6 @@ class BSplineController():
         else:
             self.selection = (s+1, 0)[int(bool(s>n-2))] #wrap around to 0 if s is n-1           
     
-
     def reset_control_pts(self):
         """
         reset control points to original values
@@ -129,7 +124,6 @@ class BSplineController():
                 self.shift_active = True
         # self.print_status()
 
-
     def on_release(self,key):
         """
         callback for key release
@@ -138,23 +132,23 @@ class BSplineController():
         """
         if key == keyboard.Key.shift:
             self.shift_active = False
-        self.recompute() #recompute the spline if enough time has passed since the last updates
+        self.update() #recompute the spline if enough time has passed since the last updates
         self.print_status()
 
     def print_status(self):
         """
         print the current position of all control points and show which is selected
         """
-        # control_pts_str = ''
-        # for i in range(len(self.control_pts)):
-        #     if i == self.selection:
-        #         control_pts_str += '\033[1m' #make boldface
-        #     control_pts_str += '{}: {:0.2f}, {:0.2f}, {:0.2f} \033[0m \n'.format(i,*self.control_pts[i])
+        control_pts_str = ''
+        for i in range(len(self.control_pts)):
+            if i == self.selection:
+                control_pts_str += '\033[1m' #make boldface
+            control_pts_str += '{}: {:0.2f}, {:0.2f}, {:0.2f} \033[0m \n'.format(i,*self.control_pts[i])
         print('\n'+ 
-                # 'shift: {}'.format(('OFF','ON')[self.shift_active])+'\n'+
-                'selected control point: {}'.format(self.selection))
-                # 'control points: \n'+
-                # control_pts_str+'\r')
+                'shift: {}'.format(('OFF','ON')[self.shift_active])+'\n'+
+                'selected control point: {}'.format(self.selection)+
+                'control points: \n'+
+                control_pts_str+'\r')
         
 
     def compute_bsplines(self,core,control_pts):
@@ -166,10 +160,10 @@ class BSplineController():
         """
         k = core.order
         M = core.get_blending_matrix(k) 
-        t = core.t
-        n = len(t)
+        n = core.res
         l = len(control_pts) 
         N = n*(l-k) #total number of points in (each) spline
+        scale = self.timescale
         print('n: {}  l: {}  k: {}  N: {}'.format(n,l,k,N))
         
 
@@ -182,15 +176,15 @@ class BSplineController():
         pos_bspline = np.zeros((N,3)) 
         vel_bspline = np.zeros((N,3))
         acc_bspline = np.zeros((N,3))
-        
+
         for i in range(l-k): #could probably make this whole thing a matrix operation somehow (make giant diagonal matrix...i've done this before))
             #also, after the first time, you only have to update sections of the bspline each time, not recompute the whole thing
             pts = control_pts[i:i+k+1,:] #sliding window of (k+1) control points
             lower = i*n
             upper = (i+1)*n
             pos_bspline[lower:upper,:] = np.linalg.multi_dot((pos_basis,M,pts)) #(n x k1)*(k1 x k1)*(k1 x 3)=(n x 3), where k1 = order+1
-            vel_bspline[lower:upper,:] = np.linalg.multi_dot((vel_basis,M,pts))
-            acc_bspline[lower:upper,:] = np.linalg.multi_dot((acc_basis,M,pts))
+            vel_bspline[lower:upper,:] = np.linalg.multi_dot((vel_basis,M,pts))*(1.0/scale)
+            acc_bspline[lower:upper,:] = np.linalg.multi_dot((acc_basis,M,pts))*(1.0/scale)**2
 
        # ======================================================================================================================
         #better implemntation
@@ -199,21 +193,12 @@ class BSplineController():
         #do the whole thing the first time, then only change sections (lower and upper bounds are neighborhood of the changed point)
         # bspline[i,:,:] = np.linalg.multi_dot((basis[i,:,:],M,pts))
         # ======================================================================================================================
-
-
-        # do evaluation stuff somewhere
-        # vel_norms = np.linalg.norm(vel_bspline,axis=1)
-        # max_vel = np.max(vel_norms)
-        # acc_norms = np.linalg.norm(acc_bspline,axis=1)
-        # max_acc = np.max(acc_norms)
-        # print('max vel: {}'.format(max_vel))
-        # print('max acc: {}'.format(max_acc))
         
         self.last_update = time.time()
         return pos_bspline, vel_bspline, acc_bspline
     
 
-    def recompute(self):
+    def update(self):
         """
         recompute the bspline if enough time has passed since the last update
         """
@@ -222,13 +207,20 @@ class BSplineController():
 
         now = time.time()
         prev = self.last_update
-        m = self.min_update_time
-        if now - prev > m:
-            print('\nrecomputing...')
-            p,v,a = self.compute_bsplines(core,control_pts)
-            self.pos_bspline = p
-            self.vel_bspline = v
-            self.acc_bspline = a
+        # m = self.min_update_time
+        # if now - prev > m:
+
+        p,v,a = self.compute_bsplines(core,control_pts)
+        self.pos_bspline = p
+        self.vel_bspline = v
+        self.acc_bspline = a
+
+        m = utils.centroid(p)
+        M = m.reshape((1,3)).repeat(len(p),axis=0)
+        self.rotations = utils.rotation_align_axis('z',v,grounded_axis='x',flip=False)
+
+        # is slicing the whole array faster than re-assigning it??? test this
+        #     self.pos_bspline[:,:]=p    ~vs~   self.pos_bspline=p
 
     def keyboard_start(self):
         """
@@ -244,6 +236,7 @@ class BSplineController():
         listener.stop()
         listener.join()
 
+
     def update_core():
         pass
 
@@ -251,6 +244,7 @@ class BSplineController():
 if __name__ == '__main__':
     b = BSplineController()
     b.keyboard_start()
+    
 
 
 
