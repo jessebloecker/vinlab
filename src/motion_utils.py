6ffbfd79e50406c0d1ff8a9628f1ae7e193afc8c
Jesse Bloecker <jesseb@udel.edu>
2023-12-27 13:05:48 -0500

    add more functions

--- src/motion_utils.py
+++ src/motion_utils.py
@@ -37,11 +37,17 @@ def rotation_align_axis(axis,vec,grounded_axis=None,flip=False):
         flip: bool, if True, the grounded axis will be negated, giving the other available solution
     returns:
         R: rotation matrix where columns are the unit vectors of aligned frame 
-           expressed in the same frame as 'axis'. If multiple vectors are provided,
+           expressed in the same frame as provided vectors. If multiple vectors are provided,
            R will be a 3d array with shape (3,3,n)
 
     use: a1 = axis, a2 = grounded_axis, a3 = last_axis
     """
+    n = vec.shape[0]
+    a1 =np.zeros((n,3))
+    a2 =np.zeros((n,3))
+    a3 =np.zeros((n,3))
+    R = np.zeros((n,3,3))
+
     remaining = [0,1,2] #axis numbers corresponding to 'xyz'
     try:
         axis_num = 'xyz'.index(axis)
@@ -58,26 +64,21 @@ def rotation_align_axis(axis,vec,grounded_axis=None,flip=False):
         print('axis names must be \'x\',\'y\', or \'z\'')
         raise 
 
-    if vec.ndim==1:
-        vec = vec.reshape((1,3))
-    n = vec.shape[0]
+    a1[:,:] = unit(vec) #first axis
     
-    a1 = unit(vec) #first axis
-    a2 = (-1.0 if flip else 1.0)*np.vstack((a1[:,1],-a1[:,0],np.zeros(n))).T #second axis ("grounded axis")
-
-    positive_seq=['xy','yz','zx'] #ensure the result of the cross product will be a right-handed frame
-    sign = 1.0 if axis+grounded_axis in positive_seq else -1.0
-    a3 = sign*np.cross(a1,a2)
+    a2[:,0] =  a1[:,1] 
+    a2[:,1] =  -a1[:,0]
+    a2[:,:] = (-1.0 if flip else 1.0)*unit(a2)  #second axis ("grounded_axis")
 
     positive_seq = bool(axis+grounded_axis in ['xy','yz','zx'])
-    a3 = (1.0 if positive_seq else -1.0)*np.cross(a1,a2)
+    a3[:,:] = (1.0 if positive_seq else -1.0)*np.cross(a1,a2) #third axis
 
-
-    R = np.zeros((n,3,3))
     R[:,:,axis_num] = a1
     R[:,:,grounded_num] = a2
     R[:,:,last_num] = a3
 
+    # for i in np.arange(n): #debug
+    #     print('i: {} a1: {} a2: {} a3: {}'.format(i,a1[i],a2[i],a3[i]))
     return np.squeeze(R)
     
 
@@ -131,27 +132,22 @@ def angvel_from_rotations(rot,dt):
     """
     R = rot
     n = len(R)
-    rotvec = np.zeros((n,3)) #to hold relative rotations represented as scaled axes
-    angvel = np.zeros((n,3))
-    body_angvel = np.zeros((n,3))
-
+    R_rel = np.zeros((n,3,3))
+    
     n_is_odd = bool(n%2)
-    if n_is_odd: #store last rotation separately to handle later, make n even
-        R_last = R[n-1] 
+    if n_is_odd: #reduce n by 1, then correct for endpoints later
         n = n-1  
 
-    k = int(n/2)  #half the number of rotations 
+    k = int(n/2)   #half the number of rotations 
     R_e = R[0::2]  #rotations at even indices - shape (k,3,3))
-    R_o = R[1::2]   #rotations at odd indices - shape (k,3,3))
+    R_o = R[1::2]  #rotations at odd indices - shape (k,3,3))
 
     i = 0
     for R_half in (R_e,R_o): 
         block = block_diag(*R_half[0:k-1])
         stack = stack_2d(R_half[1:k])
         rel = (block.T@stack).reshape(k-1,3,3)
-
-        # 'interlace' the relative rotations back into one (n,3) array
-        rotvec[1:n-1][i::2] = Rotation.from_matrix(rel).as_rotvec() 
+        R_rel[1:n-1][i::2] = rel # 'interlace' the relative rotations back into one (n,3,3) array
         i+=1
 
     # now handle the endpoints
@@ -159,12 +155,21 @@ def angvel_from_rotations(rot,dt):
         n+=1
         # the previously last rotation is now the second to last rotation
         # compute relative rotation at n-2 exactly as above
-        rotvec[n-2] = Rotation.from_matrix(R[n-3].T@R[n-1]).as_rotvec()
+        R_rel[n-2] = R[n-3].T@R[n-1]
+    R_rel[0] = R[0].T@R[1] #forward difference
+    R_rel[n-1] = R[n-2].T@R[n-1] #backward difference
 
-    rel_first = R[0].T@R[1] #forward difference
-    rel_last = R[n-2].T@R[n-1] #backward difference
-    rotvec[0] = Rotation.from_matrix(rel_first).as_rotvec()
-    rotvec[n-1] = Rotation.from_matrix(rel_last).as_rotvec()
+
+
+    rotvec = Rotation.from_matrix(R_rel).as_rotvec()
+    angvel = np.zeros((n,3))
+    body_angvel = np.zeros((n,3))
+
+    # for i in range(0,len(rotvec)):
+    #     u = unit(rotvec[i])
+    #     u_prev = unit(rotvec[i-1])
+    #     print('{}: axis {}, diff {} angle {}'.format(i,u,np.linalg.norm(u-u_prev),np.rad2deg(np.linalg.norm(rotvec[i]))))
+
 
     body_angvel[0] = (1/dt)*rotvec[0]
     body_angvel[1:n-1,:] = (1/(2*dt))*rotvec[1:n-1,:]
@@ -201,20 +206,33 @@ def repeat_3d(arr,n):
 
 def in_frame(rot,vec):
     """
-    rotate each vector in 'vec' by the inverse of corresponding rotation in 'rot'
+    multiply each vector in 'vec' by the inverse of corresponding rotation in 'rot'
     params:
         vec: array (n,3)
         rot: array (n,3,3) 
     returns:
         vec_rotated: array (n,3) where vec_rotated[i,:] = rot[i,:,:].T@vec[i,:]
     """
-    if not rot.shape[0] == vec.shape[0]:
-        raise ValueError('\'rot\' and \'vec\' must have the same number of rows')
+    if vec.ndim == 1:
+        vec = vec[np.newaxis,:].repeat(len(rot),axis=0)
+    
     
     R = block_diag(*rot)
     vec_rotated = (R.T@vec.flatten()).reshape(vec.shape)
     return vec_rotated
 
+def skew(vec):
+    """
+    compute skew symmetric matrix from vector
+    params:
+        vec: array of length 3
+    returns:
+        skew symmetric matrix of shape (3,3)
+    """
+
+    return np.array([[0,-vec[2],vec[1]],
+                     [vec[2],0,-vec[0]],
+                     [-vec[1],vec[0],0]])
 
 
 def as_scipy_rotation(rot):
@@ -228,15 +246,88 @@ def as_scipy_rotation(rot):
                                 with convention x --> y --> z in fixed frame
                                 (equivalent to z --> y --> x in body frame)
             1x1 scalar, assume yaw in radians
-"""
-    N = len(rot)
+    """
+    n = len(rot)
     dim = rot.shape
-    if dim == (4,) or dim == (N,4):
-        out = Rotation.from_quat(rot)
-    elif dim == (3,3) or dim == (N,3,3):
+    if dim == (4,) or dim == (n,4):
+        out = Rotation.from_quat(rot) #assumes hamilton
+    elif dim == (3,3) or dim == (n,3,3):
         out = Rotation.from_matrix(rot)
-    elif dim == (3,) or dim == (N,3):
+    elif dim == (3,) or dim == (n,3):
         out = Rotation.from_euler('xyz', rot)
     else:
         raise ValueError('Invalid rotation input with shape {}'.format(dim))
     return out
+
+def q_conjugate(q):
+    """
+    Compute the conjugate of quaternion(s) in [x,y,z,w] format.
+    Equivalent to switching between Hamilton and JPL conventions
+    params:
+        q: (4,) or (4,n) array
+    returns:
+        q_conj = [-x,-y,-z,w]
+    """
+    n = len(q)
+    if not q.shape in [(4,), (4,n)]:
+        raise ValueError('\'q\' must have shape (4,) or (4,n), got shape {}, n={}'.format(q.shape,n))
+    
+    q_conj = np.zeros(q.shape)
+    q_conj[0:3] = -q[0:3]
+    q_conj[3] = q[3]
+    return q_conj
+    
+    
+def config_transform(config):
+    rotation  = np.array(config['rotation']).astype(np.float32)
+    R = as_scipy_rotation(rotation).as_matrix()
+    pos = np.array(config['translation']).astype(np.float32)
+    return (R,pos)
+
+
+def attribute_print(obj,indent=1,object_name=None):
+    if object_name is None:
+        object_name = obj.__class__.__name__
+    PURPLE = "\033[0;35m"
+    YELLOW = "\033[1;33m"
+    BOLD = "\033[1m"
+    END = "\033[0m"
+    space='    '
+    print((indent-1)*space+YELLOW+str(object_name)+END)
+    for k, v in obj.__dict__.items():
+        if v.__class__.__module__=='numpy':
+            print(indent*space+BOLD+'{}: '.format(k)+END+
+                    PURPLE+'{} '.format(v.shape)+END+'{}'.format(v.dtype))
+        elif isinstance(v,list):
+            print((indent)*space+YELLOW+str(k.title())+END)
+            for i in range(len(v)):
+                attribute_print(v[i],indent+2,object_name='_')
+        elif not v.__class__.__module__=='builtins':
+            attribute_print(v,indent+1)
+        else:
+            print(indent*space+BOLD+'{}'.format(k)+END+': {}'.format(v))
+
+def init_trajectory_timestamps(n, t=None, dur=None):
+    """
+    initialize timestamps array from either given time stamps or duration
+    params:
+        n: number of data points
+        t: array of time stamps
+        dur: duration of trajectory
+    returns:
+        tuple (_t,_dur), where
+            _t = timestamps array of shape (n,)
+            _dur = duration (difference between first and last time stamps)
+    """
+    if t is not None:
+        if len(t)==n:
+            _t = t
+            _dur = float(_t[-1]-_t[0]) #ignore provided 'dur'
+        else:
+            raise ValueError('number of time stamps ({}) does not match number of data points ({})'.format(len(t),n))
+    elif dur is not None:
+        _t = np.linspace(0,dur,n)
+        _dur = float(dur)
+    else:
+        raise ValueError('must provide either a duration or an array of time stamps')
+    return _t, _dur
