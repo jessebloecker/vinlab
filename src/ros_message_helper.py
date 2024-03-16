ffda817c2e4e10c41fe296e0aeb49be8e4c4512b
Jesse Bloecker <jesseb@udel.edu>
2024-03-15 21:20:19 -0400

    add color message and other updates

--- src/ros_message_helper.py
+++ src/ros_message_helper.py
@@ -15,49 +15,78 @@ import motion_utils as utils
 Convenience functions to help fill ROS2 messages fields
 """
 
+def as_color_msg(color, scale=1.0):
+    # print('rmh: color: {}'.format(color))
+    c = ColorRGBA()
+    color = np.array(color).astype(float)/scale
+    if len(color) == 3:
+        color = np.append(color,1.0)
+    c.r, c.g, c.b, c.a = color
+    return c
+
 def as_header(stamp,frame_id):
     h = Header()
     h.stamp = as_stamp(stamp)
     h.frame_id = frame_id
     return h
 
-def as_marker_msg(stamp,frame_id,pos,*,id=0, rot=None,
-                   scale=3*[0.25], color=[1.,0.,0.,0.9], _type=Marker.SPHERE):
+
+def as_int16multiarray_msg(arr):
+    i = Int16MultiArray()
+    i.data = arr
+    return i
+
+def as_marker_msg(stamp=0.0,frame_id='',pos=None,*,marker_id=0, rot=None, marker_type=2,
+                   scale=3*[0.25], color=[1.,0.,0.,0.9], points=None):
     m = Marker()
     m.header.stamp = as_stamp(stamp)
-    m.id = id
+    m.id = marker_id
     m.header.frame_id = frame_id
-    m.type = _type
     m.scale.x, m.scale.y, m.scale.z = scale
     m.color.r, m.color.g, m.color.b, m.color.a = color
-    m.pose.position = as_point_msg(pos)
+    m.type = marker_type
+    if pos is not None:
+        m.pose.position = as_point_msg(pos)
     if rot is None:
         m.pose.orientation.w = 1.0
     else:
         m.pose.orientation = as_quaternion_msg(rot)
-        
-
+    
+    if points is not None:
+        if marker_type is not None:
+            m.type = marker_type
+        else:
+            m.type = Marker.LINE_STRIP
+            m.points = [as_point_msg(p) for p in points]
     return m
 
-def as_markerarray_msg(stamp,frame_id,pos):
+def as_markerarray_msg(stamp=0.0,frame_id='',pos=None, *,n=None, marker_type=2, scale=3*[0.25]):
     ma = MarkerArray()
-    for i in range(len(pos)):
-        m = as_marker_msg(stamp,frame_id,pos[i],id=i)
-        ma.markers.append(m)
+    if pos is not None:
+        for i in range(len(pos)):
+            m = as_marker_msg(stamp,frame_id,pos[i],marker_id=i, marker_type=marker_type)
+            ma.markers.append(m)
+    elif n is not None:
+        for i in range(n):
+            m = as_marker_msg(stamp,frame_id,marker_id=i, marker_type=marker_type, scale=scale)
+            ma.markers.append(m)
     return ma
 
-def as_path_msg(stamps,frame_id,pos,rot=None):
+def as_path_msg(stamps=None,frame_id='',pos=None,rot=None):
     p = Path()
-    p.header.stamp = as_stamp(stamps[0])
+    if stamps is not None:
+        p.header.stamp = as_stamp(stamps[0])
     p.header.frame_id = frame_id
     if rot is None:
-        rot = np.tile([0.,0.,0.,1.],(len(pos),1))
-    p.poses = [as_posestamped_msg(stamps[i],frame_id,pos[i],rot[i]) for i in range(len(pos))]
+        # rot = np.tile([0.,0.,0.,1.],(len(pos),1))
+        rot = [0.,0.,0.,1.]
+    if pos is not None:
+        p.poses = [as_posestamped_msg(stamps[i],frame_id,pos[i],rot) for i in range(len(pos))]
     return p
 
 def as_point_msg(pos):
     p = Point()
-    p.x, p.y, p.z = pos
+    p.x, p.y, p.z = np.array(pos).astype(float)
     return p
 
 def as_point32_array(arr):
@@ -78,12 +107,15 @@ def as_pointcloud_msg(stamp, frame_id, points):
     p.points=points
     return p
 
-def as_posestamped_msg(stamp,frame_id,pos,rot):
+def as_posestamped_msg(stamp=0.0,frame_id='',pos=None,*,rot=None):
     p = PoseStamped()
     p.header.stamp = as_stamp(stamp)
     p.header.frame_id = frame_id
     p.pose.position = as_point_msg(pos)
-    p.pose.orientation = as_quaternion_msg(rot)
+    if rot is None:
+        p.pose.orientation.w = 1.0
+    else:
+        p.pose.orientation = as_quaternion_msg(rot)
     return p
 
 def as_quaternion_msg(rot):
